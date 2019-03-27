32170b11e7ce1de9c04b4b892dbbd6df34a32f9a
Carly Pecora <carlyannpecora@gmail.com>
2019-03-27 19:55:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.querySelector("#nested .target")
+}
+
+function increaseRankBy(n){
+ var lis = document.querySelectorAll(".ranked-list li")
+ for(var i = 0; i < lis.length; i++){
+   lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+ }
+}
+
+function deepestChild(){
+  var nests = document.querySelectorAll("#grand-node div")
+  for(var i = 0; i < nests.length; i++){
+    if(nests[i].children.length === 0){
+      return nests[i]
+    }
+  }
+}
